1a86911b275a210e530842ab0e3cf69c7b3a9cc9
Joel Ugwumadu <joel@casava.co>
2022-08-19 19:28:07 +0100

    remove commet

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -2,7 +2,6 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config()
 
-// Collection is deployed to 0x275312D806331437cBE8C89a9B05C8c2E5300056
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
